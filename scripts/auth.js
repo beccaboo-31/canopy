16c4af41a0cb40b9cd059069b2172d300f78f46f
shivam1708 <shivam.pawase@somaiya.edu>
2019-10-05 19:45:42 +0530

    init 2

--- scripts/auth.js
+++ scripts/auth.js
@@ -50,7 +50,9 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-  auth.signOut();
+  auth.signOut().then(()=>{
+    console.log("User logged out!")
+  });
 });
 
 // login
