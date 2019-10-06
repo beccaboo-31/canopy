db5a8d68826081c80265317ac64ead2ec5ed3e73
shivam1708 <shivam.pawase@somaiya.edu>
2019-10-06 08:31:59 +0530

    chatroom fixes

--- scripts/chats.js
+++ scripts/chats.js
@@ -3,6 +3,13 @@ const chatroom = document.querySelector('#title');
 
 //console.log(chatroom.innerHTML);
 
+
+var urlString = window.location.href;
+console.log(urlString);
+urlParams = parseURLParams(urlString);
+chatroom.innerHTML = urlParams["chatroomname"][0];
+console.log(urlParams["chatroomname"][0]);
+
 db.collection(chatroom.innerHTML).onSnapshot(snapshot => {
   console.log("reached here");
   setupchats(snapshot.docs);
@@ -17,6 +24,29 @@ message.addEventListener('submit', (e) => {
     user:auth.currentUser.email
   });
 });
+
+
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+
+
 
 const chatroom_messages = document.querySelector('#chatroom-messages');
 
